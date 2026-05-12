4229f8e18db51d9ac384c8b88c2182bcd1bbb02b
vinter213 <daniel134745@gmail.com>
2026-05-12 13:06:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ window.RPC_API_URL = "https://rpc-team-crm.onrender.com";
 
 // Кнопки связи.
 // Поменяй на свои реальные ссылки.
-window.RPC_DISCORD_URL = "https://discord.gg/REPLACE_ME";
+window.RPC_DISCORD_URL = "https://discord.gg/4GHqJFX9RT";
 window.RPC_TELEGRAM_URL = "https://t.me/REPLACE_ME";
